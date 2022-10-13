f9e02bfdecc01861d6ffd0ef01459fa2b44b3540
Abhishek Mukherjee <abhishekmukherjee@matrixnmedia.com>
2022-10-13 18:29:25 +0530

    full page design

--- src/app/component/slider-content/slider-content.component.ts
+++ src/app/component/slider-content/slider-content.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-slider-content',
   templateUrl: './slider-content.component.html',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SliderContentComponent implements OnInit {
 
-  constructor() { }
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 10000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }
 
   ngOnInit(): void {
   }
-  images = [944, 1011, 984, 932, 1018].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  //images = [944, 1011, 984, 932, 1018].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
